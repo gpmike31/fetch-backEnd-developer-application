518d6bdfddd2449f2903c41e37b1ad9ffc5b9654
gpmike31 <gpmike3105@gmail.com>
2022-05-10 20:17:28 -0500

    updated comments for points schema model

--- models/Points.js
+++ models/Points.js
@@ -1,4 +1,33 @@
 //insert points schema, will require mongoose
+const {Schema, model}= require("mongoose")
+const PayerSchema=require("./Payer")
+
+//points schema will show the amount of points as a string whether adding or subtracting
+//and will show the current date and time for each transaction.
+//This schema below will reference the payers module inside of an array and use a virtual
+//to return the payers in a string
+const PointsSchema=new Schema(
+    {
+        pointText:{
+            type:String,
+            trim:true,
+            required:"Current Point Total"
+        },
+        createdAt:{
+            type:Date,
+            default:Date.now
+        },
+        payers:['${PayerSchema}']
+    }
+)
+
+PointsSchema.virtual("payers").get(function(){
+    console.log(this.payers);
+    return this.payers.String
+})
+
+const Points = model("Points", PointsSchema)
+module.exports=Points
 //will need to reference payer schema
 
 
