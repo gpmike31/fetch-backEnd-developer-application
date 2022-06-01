b3be00b7c0c0a1589a96d3d02243970a5d8bc158
gpmike31 <gpmike3105@gmail.com>
2022-05-31 22:41:13 -0500

    need to figure out when running seeds why parameters are undefined

--- models/index.js
+++ models/index.js
@@ -5,19 +5,12 @@ const Points = require('./Points');
 //Points belongsTo Payer
 Points.belongsTo(Payer, {
     foreignKey: 'payers_id',
-    onDelete: 'SET NULL'
+    onUpdate: 'SET NULL'
 })
 
 Payer.hasMany(Points, {
     foreignKey: 'payers_id',
     onUpdate: 'SET NULL'
-})
-
-Points.belongsToMany(Payer, {
-    through: Payer,
-    foreignKey: 'payers_id',
-    onUpdate: 'SET NULL'
-
 })
 
 module.exports = {
